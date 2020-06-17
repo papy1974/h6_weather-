5899b30dada8854123ed234a34ac2954cbd39f3d
jessica <papylotomba@yahoo.fr>
2020-06-17 13:38:28 -0400

    make ready adjust Date

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ searchButton.click(function () {
             // .addClass("card-text");
             currentCard.append(currentName);
 
-            // Adjust Date 
+            // make ready adjust Date 
             var timeUTC = new Date(response.dt * 1000);
             currentName.append(response.name + " " + timeUTC.toLocaleDateString("en-US"));
             currentName.append(`<img src="https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png">`);
